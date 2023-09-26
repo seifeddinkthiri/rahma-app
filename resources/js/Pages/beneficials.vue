fd665bd1387a0baaae29ac936fe0f2f1199726fe
Mosbeh303 <mesbahhamdidsi@gmail.com>
2023-09-26 18:39:53 +0200

    added empty list row in beneficial index table

--- resources/js/Pages/beneficials.vue
+++ resources/js/Pages/beneficials.vue
@@ -55,7 +55,7 @@
     </div>
     <div class="flex items-center justify-between mb-4 mt-4">
       <h1 class="text-xl font-bold" v-if="families.data.length > 0">قائمة المنتفعين</h1>
-      <h1 class="text-xl font-bold" v-else>قائمة فارغة</h1>
+      <h1 class="text-xl font-bold" v-else></h1>
 
       <button @click="show_modal = true" class="btn-indigo">
         <span> إضافة منتفع</span>
@@ -78,29 +78,29 @@
         <tbody class="text-right">
           <tr v-for="family in families.data" :key="family.id" class="hover:bg-gray-100 focus-within:bg-gray-100">
             <td class="border-t" v-if="family.name">
-              <Link  class="flex items-center px-6 py-4 focus:text-indigo-500" :href="`/families/${family.id}/edit`">
+              <Link class="flex items-center px-6 py-4 focus:text-indigo-500" :href="`/families/${family.id}/edit`">
                 <p class="h-4">{{ family.name }}</p>
               </Link>
             </td>
             <td class="border-t" v-if="family.name">
-              <Link  class="flex items-center px-6 py-4 focus:text-indigo-500" :href="`/families/${family.id}/edit`">
+              <Link class="flex items-center px-6 py-4 focus:text-indigo-500" :href="`/families/${family.id}/edit`">
                 <p class="h-4">{{ family.address }}</p>
               </Link>
             </td>
             <td class="border-t" v-if="family.name">
-              <Link  class="flex items-center px-6 py-4 focus:text-indigo-500" :href="`/families/${family.id}/edit`">
+              <Link class="flex items-center px-6 py-4 focus:text-indigo-500" :href="`/families/${family.id}/edit`">
                 <p class="h-4">{{ family.phone }}</p>
               </Link>
             </td>
             <td class="border-t" v-if="family.name">
-              <Link  class="flex items-center px-6 py-4 focus:text-indigo-500" :href="`/families/${family.id}/edit`">
+              <Link class="flex items-center px-6 py-4 focus:text-indigo-500" :href="`/families/${family.id}/edit`">
                 <p class="h-4" v-if="family.status == 'active'">نشط</p>
                 <p class="h-4" v-if="family.status == 'inactive'">غير نشط</p>
                 <p class="h-4" v-if="family.status == 'disabled'">محضور</p>
               </Link>
             </td>
             <td class="border-t" v-if="family.name">
-              <Link  class="flex items-center px-6 py-4 focus:text-indigo-500" :href="`/families/${family.id}/edit`">
+              <Link class="flex items-center px-6 py-4 focus:text-indigo-500" :href="`/families/${family.id}/edit`">
                 <p class="h-4" v-if="family.social_status == 'widow'">أرملة</p>
                 <p class="h-4" v-if="family.social_status == 'divorced'">مطلقة</p>
                 <p class="h-4" v-if="family.social_status == 'single_mother'">أم عزباء</p>
@@ -109,12 +109,10 @@
               </Link>
             </td>
             <td class="border-t" v-if="family.name">
-
-              <Link  v-if="family.photo"   class="flex items-center px-6 py-4 focus:text-indigo-500" :href="`/families/${family.id}/edit`">
+              <Link v-if="family.photo" class="flex items-center px-6 py-4 focus:text-indigo-500" :href="`/families/${family.id}/edit`">
                 <img class="block -my-2 mr-2 w-10 h-8 rounded" :src="'uploads/' + family.photo" />
-
               </Link>
-              <Link v-else  class="flex items-center px-6 py-4 focus:text-indigo-500" :href="`/families/${family.id}/edit`"> لا يوجد </Link>
+              <Link v-else class="flex items-center px-6 py-4 focus:text-indigo-500" :href="`/families/${family.id}/edit`"> لا يوجد </Link>
             </td>
             <td class="w-px border-t" v-if="family.name">
               <Link class="flex items-center px-4" :href="`/members/${family.id}/create_new_one`" tabindex="-1">
@@ -126,22 +124,38 @@
 
             <td class="w-px border-t" v-if="family.name">
               <div class="flex items-center">
-                <Link  class="flex items-center px-4" :href="`/families/${family.id}/edit`" tabindex="-1">
+                <Link class="flex items-center px-4" :href="`/families/${family.id}/edit`" tabindex="-1">
                   <icon name="cheveron-right" class="block w-4 h-4 fill-gray-400" />
                 </Link>
-                <Link  class="flex items-center px-4" :href="`/families/${family.id}/show`" tabindex="-1">
+                <Link class="flex items-center px-4" :href="`/families/${family.id}/show`" tabindex="-1">
                   <icon name="eye" />
                 </Link>
-                <button v-if="!family.deleted_at"  class="flex items-center px-4" tabindex="-1" @click="delete_family(family.id)">
+                <button v-if="!family.deleted_at" class="flex items-center px-4" tabindex="-1" @click="delete_family(family.id)">
                   <icon name="delete" />
                 </button>
-                <button v-else  class="flex items-center px-4" tabindex="-1" @click="restore(family.id)">
+                <button v-else class="flex items-center px-4" tabindex="-1" @click="restore(family.id)">
                   <icon name="restore" />
                 </button>
               </div>
             </td>
           </tr>
         </tbody>
+      </table>
+    </div>
+    <div class="bg-white rounded-md shadow overflow-x-auto" v-else>
+      <table class="w-full">
+        <tr class="text-right font-bold">
+            <th class="pb-4 pt-6 px-6">المعيل</th>
+            <th class="pb-4 pt-6 px-6">العنوان</th>
+            <th class="pb-4 pt-6 px-6">الهاتف</th>
+            <th class="pb-4 pt-6 px-6">الحالة</th>
+            <th class="pb-4 pt-6 px-6">الحالة المدنية</th>
+            <th class="pb-4 pl-3 pt-6">الصورة</th>
+            <th class="pb-4 pl-3 pt-6" colspan="3">إجراءات</th>
+          </tr>
+        <tr v-if="families.data.length === 0">
+            <td class="px-6 py-4 border-t" colspan="6">قائمة فارغة</td>
+          </tr>
       </table>
     </div>
 
